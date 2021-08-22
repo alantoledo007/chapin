c4726ce80e6b5bc6854c1223f5b26d1825d2bb39
Alan Toledo <alantoledo.work@gmail.com>
2021-08-21 23:46:45 -0300

    testing deploy if CI fails

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello word</h1>
+        <h1>Hello word!</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn R
         </a>
       </header>
     </div>
